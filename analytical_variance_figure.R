b8a411456cf9185cbc6dcc7d32a5dbb08e24fc55
Avi Kenny <avi.kenny@gmail.com>
2024-11-26 12:37:07 -0500

    Updated analytic figure

--- analytical_variance_figure.R
+++ analytical_variance_figure.R
@@ -1,13 +1,16 @@
 # 1. load simulation results and packages, create helper functions ####
 
 library(SimEngine)
+library(stringr)
+library(ggplot2)
+library(dplyr)
 
-load("simulation_results.RData")
+# load("simulation_results.RData")
 
 # get SD from ICC 
-get_sd <- function(icc) {
+get_sd <- function(icc, sigma) {
   
-  sd <- sqrt(icc/(1-icc))
+  sd <- sigma*sqrt(icc/(1-icc))
   return(sd)
   
 }
@@ -15,21 +18,23 @@ get_sd <- function(icc) {
 # 2. create analytical dataset ####
 
 # varying m, assuming 100 clusters
-m <- c(10, 100)
-icc <- sim$levels$icc
+m <- c(10, 25)
+n <- 1000
+icc <- seq(from = 0, to = 0.2, by = 0.05)
+sigma <- sqrt(1000/8)
 
 analytical_disc_varying_m <- expand.grid(m = m, icc = icc) %>% 
-  mutate(var = 8/(m*100),
+  mutate(var = (8*sigma^2)/n,
          method = 'Analytical',
          design = 'DISC')
 
 analytical_rcs_varying_m <- expand.grid(m = m, icc = icc) %>% 
-  mutate(var = 8*(m*(get_sd(icc)^2) + 1)/(m*100),
+  mutate(var = 8*(m*(get_sd(icc,sigma)^2) + sigma^2)/n,
          method = 'Analytical',
          design = 'Traditional RCS')
 
 # 3. create figure to compare designs for analytical calculations across different ICC values ####
-custom_labels <- c("10" = "10 individuals\nper cluster", "100" = "100 individuals\nper cluster")
+custom_labels <- c("10" = "10 individuals\nper cluster", "25" = "25 individuals\nper cluster")
 (analytical_variance_figure <- analytical_disc_varying_m %>% 
     rbind(analytical_rcs_varying_m) %>% 
     ggplot(aes(icc, var, linetype = design)) +
@@ -37,12 +42,13 @@ custom_labels <- c("10" = "10 individuals\nper cluster", "100" = "100 individual
     facet_wrap(~ m, labeller = as_labeller(custom_labels)) + 
     xlab('ICC') +
     ylab('Total variance') + 
+    scale_y_continuous(breaks=c(1:10)) +
     labs(title = str_wrap('Total analytical variance, comparing DISC and traditional RCS designs'),
          linetype = 'Design') 
 )
 
-# 4. save ####
-ggsave("analytical_variance_figure.pdf",
-       plot = analytical_variance_figure,
-       width = 9,
-       height = 5)
+# # 4. save ####
+# ggsave("analytical_variance_figure.pdf",
+#        plot = analytical_variance_figure,
+#        width = 9,
+#        height = 5)
